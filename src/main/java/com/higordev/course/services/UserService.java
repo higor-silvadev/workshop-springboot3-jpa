6adffd131bce9bd1fda652cb1e5f47f850d11303
higor-silvadev <151585174+higor-silvadev@users.noreply.github.com>
2025-05-05 21:57:58 -0300

    feat: service layer, component registration

---
+++ src/main/java/com/higordev/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.higordev.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.higordev.course.entities.User;
+import com.higordev.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
